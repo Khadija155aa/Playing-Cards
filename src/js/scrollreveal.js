242bf8193886a57177ef0bca87a58bc1c80f18aa
Khadija155aa <alshuwai@msu.edu>
2025-02-10 23:12:34 +0000

    add changes

--- src/js/scrollreveal.js
+++ src/js/scrollreveal.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.planet-card', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 200,
+        easing: 'ease-in-out'
+    });
+});
